ca93c28f7d2a8c8ac248a23950d7b0e71b98996c
zharabedian <zharabedian@gmail.com>
2019-06-15 12:52:40 -0400

    first commit

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,27 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+-- INSERT INTO candidates (name, age, party, state) VALUES ("Joseph Biden", 76, "Democrat", "Delaware");
+-- INSERT INTO candidates (name, age, party, state) VALUES ("Cory Booker", 50, "Democrat", "New Jersey");
+
+
+INSERT INTO candidates (name, age, party, state) VALUES ("Steve Bullock", 53, "Democrat", "Montana");
+INSERT INTO candidates (name, age, party, state) VALUES ("Pete Buttigieg", 37, "Democrat", "Indiana");
+INSERT INTO candidates (name, age, party, state) VALUES ("Julian Castro", 44, "Democrat", "Texas");
+INSERT INTO candidates (name, age, party, state) VALUES ("Bill de Blasio", 58, "Democrat", "New York");
+INSERT INTO candidates (name, age, party, state) VALUES ("John Delaney", 54, "Democrat", "Maryland");
+INSERT INTO candidates (name, age, party, state) VALUES ("Tulsi Gabbard", 38, "Democrat", "Hawaii");
+INSERT INTO candidates (name, age, party, state) VALUES ("Kristen Gillibrand", 52, "Democrat", "New York");
+INSERT INTO candidates (name, age, party, state) VALUES ("Kamala Harris", 54, "Democrat", "California");
+INSERT INTO candidates (name, age, party, state) VALUES ("John Hickenlooper", 67, "Democrat", "Colorado");
+INSERT INTO candidates (name, age, party, state) VALUES ("Jay Inslee", 68, "Democrat", "Washington");
+INSERT INTO candidates (name, age, party, state) VALUES ("Amy Klobuchar", 59, "Democrat", "Minnestota");
+INSERT INTO candidates (name, age, party, state) VALUES ("Wayne Messam", 54, "Democrat", "Florida");
+INSERT INTO candidates (name, age, party, state) VALUES ("Seth Moulton", 40, "Democrat", "Massachusetts");
+INSERT INTO candidates (name, age, party, state) VALUES ("Beto O'Rourke", 46, "Democrat", "Texas");
+INSERT INTO candidates (name, age, party, state) VALUES ("Tim Ryan", 45, "Democrat", "Ohio");
+INSERT INTO candidates (name, age, party, state) VALUES ("Bernie Sanders", 77, "Democrat", "Vermont");
+INSERT INTO candidates (name, age, party, state) VALUES ("Eric Swalwell", 38, "Democrat", "California");
+INSERT INTO candidates (name, age, party, state) VALUES ("Elizabeth Warren", 54, "Democrat", "Massachusetts");
+INSERT INTO candidates (name, age, party, state) VALUES ("Marianne Williamson", 66, "Democrat", "Texas");
+INSERT INTO candidates (name, age, party, state) VALUES ("Andrew Yang", 44, "Democrat", "New York");
+
+
+
