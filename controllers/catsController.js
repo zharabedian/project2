e56f9875003b304f2cae7a86a0b573a88fc8fc2f
Erica Walsh <erica.f.walsh@gmail.com>
2019-06-18 19:29:13 -0400

    MVC updates with the team

--- controllers/catsController.js
+++ controllers/catsController.js
@@ -1,4 +1,3 @@
-// Hi, Nick. 
 var express = require("express");
 
 var router = express.Router();
@@ -8,42 +7,44 @@ var model = require("../models/cat.js");
 
 // Doesn't have username functionality
 // Need to capture username in a variable and pass it here
-router.get("/", function(req, res) {
+// 
+router.get("/:username", function (req, res) {
   res.sendFile(path.join(__dirname, "public/index.html"));
-  model.all(function(data) {
-    res.json({ cats: data });
-  });
-});
 
-// // Create all our routes and set up logic within those routes where required.
-// router.get("/data", function(req, res) {
-//   model.all(function(data) {
-//     res.json({ cats: data });
-//   });
-// });
+  var condition = req.params.username;
+
+  model.all({
+    username: username
+  },
+    (function (req, res) {
+      res.json({ res });
+    })
+)});
 
 // Add a note
-router.post("/api/add/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-  
+router.post("/api/cats/:id", function (req, res) {
+
+  var condition = "candidate_id = " + req.params.id;
+
   model.create([
     "candidate_id", "username", "notes"
   ], [
-    condition, 
-    // username would go here.  
-    req.body.notes
-  ], function(result) {
-    // Some part of the object
-    res.json({  });
-  });
+      condition,
+      // change this to come from local storage
+      req.body.username,
+      req.body.notes
+    ], function (result) {
+      // tbd. some part of the object
+      res.json({});
+    });
 });
 
-router.put("/api/add/:notes_id", function(req, res) {
+router.put("/api/add/:notes_id", function (req, res) {
   var condition = "notes_id = " + req.params.id;
 
   model.update({
     notes: req.body.notes
-  }, condition, function(result) {
+  }, condition, function (result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
@@ -53,10 +54,10 @@ router.put("/api/add/:notes_id", function(req, res) {
   });
 });
 
-router.delete("/api/add/:notes_id", function(req, res) {
+router.delete("/api/add/:notes_id", function (req, res) {
   var condition = "notes_id = " + req.params.notes_id;
 
-  cat.delete(condition, function(result) {
+  model.delete(condition, function (result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
